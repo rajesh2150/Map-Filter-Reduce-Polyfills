6c1cd979d5b7298abca9c6895ed3932390ebb645
Rajesh Korlapati <77844866+rajesh2150@users.noreply.github.com>
2024-01-05 17:46:14 +0530

    Update Script.js With Filter Polyfill

--- Script.js
+++ Script.js
@@ -10,3 +10,17 @@ Array.prototype.myMap= function (cb){
 let nums=[1,2,3];
 const multipleThree=nums.myMap((num,ind)=>{return num*3})
 console.log(multipleThree)
+
+
+//filter((num,i,arr)=>{})
+Array.prototype.MyFilter=function (cb){
+  let temp=[];
+  for(let i=0;i<this.length;i++){
+    if(cb(this[i],i,this)) temp.push(this[i])
+  }
+  return temp
+}
+
+let arr=[1,2,4,5]
+const multiple=arr.MyFilter((num)=>{return num>2})
+console.log(multiple)
