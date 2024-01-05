29742f08e35eba62c0bd79f8cc7ba6d01b93bfcc
Rajesh Korlapati <77844866+rajesh2150@users.noreply.github.com>
2024-01-05 17:54:05 +0530

    Update With Reduce Polyfill Script.js

--- Script.js
+++ Script.js
@@ -24,3 +24,15 @@ Array.prototype.MyFilter=function (cb){
 let arr=[1,2,4,5]
 const multiple=arr.MyFilter((num)=>{return num>2})
 console.log(multiple)
+
+//reduce((acc,curr,i,arr)=>{},initialValue)
+Array.prototype.MyReduce=function (cb,initialValue){
+ var accumulator=initialValue
+  for(let i=0;i<this.length;i++){
+    accumulator = accumulator ? cb(accumulator,this[i],i,this) :this[i]
+  }
+  return accumulator
+}
+let arr=[1,2,3,4]
+const multiple=arr.MyReduce((acc,curr)=>{return acc+curr})
+console.log(multiple)
